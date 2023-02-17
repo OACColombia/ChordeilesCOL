08117c4b19a799bdaaf4debe3c4488114f3a2a43
Orlando Acevedo-Charry <acevedocharry@gmail.com>
2023-02-17 10:15:22 -0500

    Update Chordeiles_COL.R
    I corrected a mistake with the figure, now it reflects the effect size regarding each specimen of reference.

--- Chordeiles_COL.R
+++ Chordeiles_COL.R
@@ -41,26 +41,44 @@ data$CI80low2 <- data$d2 - data$t80 * SE
 
 library(ggplot2)
 
-mCminorWing<-mean(data$d1[1:6])
-mCgunlaWing<-mean(data$d1[7:8])
+mCminorWingd1<-mean(data$d1[1:6])
+mCgunlaWingd1<-mean(data$d1[7:8])
 
-mCminorTail<-mean(data$d1[9:14])
-mCgunlaTail<-mean(data$d1[15:16])
+mCminorWingd2<-mean(data$d2[1:6])
+mCgunlaWingd2<-mean(data$d2[7:8])
+
+
+mCminorTaild1<-mean(data$d1[9:14])
+mCgunlaTaild1<-mean(data$d1[15:16])
+
+mCminorTaild2<-mean(data$d2[9:14])
+mCgunlaTaild2<-mean(data$d2[15:16])
+
 
 data$Measurement <- factor(data$Measurement, levels=c("Wing",
                                                       "Tail"))
 
+data$Group <- factor(data$Group, levels=c("C. g. gundlachii",
+                                          "C. g. vicinus",
+                                          "C. m. panamensis",
+                                          "C. m. chapmani",
+                                          "C. minor **",
+                                          "C. m. aserriensis",
+                                          "C. minor *",
+                                          "C. m. neotropicalis"))
+
+
 ICN<-ggplot(data,aes(x=d1,y=Group))+
   facet_wrap(~Measurement, ncol=1)+
   geom_vline(xintercept = 0)+
-  geom_vline(data=subset(data, Measurement=="Wing"),aes(xintercept = mCminorWing),linetype="dotted")+
-  geom_vline(data=subset(data, Measurement=="Tail"),aes(xintercept = mCminorTail),linetype="dotted")+
-  geom_vline(data=subset(data, Measurement=="Wing"),aes(xintercept = mCgunlaWing),linetype="dashed")+
-  geom_vline(data=subset(data, Measurement=="Tail"),aes(xintercept = mCgunlaTail),linetype="dashed")+
-  geom_linerange(aes(xmin=CI99low1,xmax=CI99upp1), color="lightgray", size=0.3125)+
-  geom_linerange(aes(xmin=CI95low1,xmax=CI95upp1), color="darkgray", size=0.625)+
-  geom_linerange(aes(xmin=CI80low1,xmax=CI80upp1), size=1.25)+
-  geom_point(shape=15, size=2.5)+
+  geom_vline(data=subset(data, Measurement=="Wing"),aes(xintercept = mCminorWingd1),linetype="dotted")+
+  geom_vline(data=subset(data, Measurement=="Tail"),aes(xintercept = mCminorTaild1),linetype="dotted")+
+  geom_vline(data=subset(data, Measurement=="Wing"),aes(xintercept = mCgunlaWingd1),linetype="dashed")+
+  geom_vline(data=subset(data, Measurement=="Tail"),aes(xintercept = mCgunlaTaild1),linetype="dashed")+
+  geom_linerange(aes(xmin=CI99low1,xmax=CI99upp1), color="gray", size=0.3125)+
+  geom_linerange(aes(xmin=CI95low1,xmax=CI95upp1), color="gray", size=1)+
+  geom_linerange(aes(xmin=CI65low1,xmax=CI65upp1), color="gray30", size=1.75)+
+  geom_point(shape=15, size=2)+
   labs(x="ICN-8501",
        y="")+
   theme_classic()+
@@ -68,14 +86,14 @@ ICN<-ggplot(data,aes(x=d1,y=Group))+
 
 IAvH<-ggplot(data,aes(x=d2,y=Group))+
   geom_vline(xintercept = 0)+
-  geom_vline(data=subset(data, Measurement=="Wing"),aes(xintercept = mCminorWing),linetype="dotted")+
-  geom_vline(data=subset(data, Measurement=="Tail"),aes(xintercept = mCminorTail),linetype="dotted")+
-  geom_vline(data=subset(data, Measurement=="Wing"),aes(xintercept = mCgunlaWing),linetype="dashed")+
-  geom_vline(data=subset(data, Measurement=="Tail"),aes(xintercept = mCgunlaTail),linetype="dashed")+
-  geom_linerange(aes(xmin=CI99low2,xmax=CI99upp2), color="lightgray", size=0.3125)+
-  geom_linerange(aes(xmin=CI95low2,xmax=CI95upp2), color="darkgray", size=0.625)+
-  geom_linerange(aes(xmin=CI80low2,xmax=CI80upp2), size=1.25)+
-  geom_point(shape=15, size=2.5)+
+  geom_vline(data=subset(data, Measurement=="Wing"),aes(xintercept = mCminorWingd2),linetype="dotted")+
+  geom_vline(data=subset(data, Measurement=="Tail"),aes(xintercept = mCminorTaild2),linetype="dotted")+
+  geom_vline(data=subset(data, Measurement=="Wing"),aes(xintercept = mCgunlaWingd2),linetype="dashed")+
+  geom_vline(data=subset(data, Measurement=="Tail"),aes(xintercept = mCgunlaTaild2),linetype="dashed")+
+  geom_linerange(aes(xmin=CI99low2,xmax=CI99upp2), color="gray", size=0.3125)+
+  geom_linerange(aes(xmin=CI95low2,xmax=CI95upp2), color="gray", size=1)+
+  geom_linerange(aes(xmin=CI65low2,xmax=CI65upp2), color="gray30", size=1.75)+
+  geom_point(shape=15, size=2)+
   facet_wrap(~Measurement, ncol=1)+
   labs(x="IAvH-A-96",
        y="")+
@@ -92,3 +110,4 @@ grid.arrange(arrangeGrob(ICN,IAvH,
                          bottom=textGrob(expression("Cohen's"~italic(d)*" Standardized effect size"),
                                          vjust = 0.5, hjust = 0.225))
 #Export pdf 6.0 * 5.5
+write.csv(data, "Anexo.csv")
